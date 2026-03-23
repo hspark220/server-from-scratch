2ab7833fc80aa689d309899f3f96f9c4b5e8fc08
Heon Soo Park <hspark220@gmail.com>
2026-03-23 19:33:10 +0900

    create a server class and Main

---
+++ version2_webserver/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    Server server = new Server("this is the name of the server...");
+    server.run();
+
+  }
+}
---
+++ version2_webserver/Server.java
@@ -0,0 +1,16 @@
+
+
+public class Server {
+
+  private string ipAddress;
+  private int portNumber;
+
+  public Server(String name) {
+    this.name = name;
+
+  }
+
+  public void run() {
+    System.out.println(name);
+  }
+}
